15e3e715d52c2754345685558924e9b5ad46ee8d
brunoksato <bruno.sato@live.com>
2014-01-16 07:11:10 -0800

    inital project, master template

---
+++ fb-friends/fb-friends/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fb-friends
+//
+//  Created by Bruno Kenji Sato Romeu on 1/16/14.
+//  Copyright (c) 2014 brunosato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
